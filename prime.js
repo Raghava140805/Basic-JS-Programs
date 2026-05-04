0b8aac41a2362175f9da361950cb5f8c7229f32c
Raghava Velamuri <raghava.velamuri@gmail.com>
2026-05-04 21:57:43 +0530

    Checking if a number is Prime using function declaration

---
+++ prime.js
@@ -0,0 +1,18 @@
+function Prime(n)
+{
+    if (n<=1)
+    {
+        return false;
+    }
+    for (let i=2; i <= Math.sqrt(n); i++)
+    {
+        if (n % i === 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+let n = 17;
+console.log(Prime(n))
